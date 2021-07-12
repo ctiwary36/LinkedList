a0a824a4b3ec5b66c033e62d3a4f295b9bef4526
Chandan Tiwary <ctiwary36@gmail.com>
2021-07-13 01:26:34 +0530

    added: UC1

---
+++ src/com/linkedlist/Main.java
@@ -0,0 +1,19 @@
+package com.linkedlist;
+
+public class Main {
+        public static void main(String[] args) {
+
+            System.out.println("Welcome to DataStructure Program");
+
+            LinkedList linkedList = new LinkedList();
+
+
+            linkedList.insert(56);
+            linkedList.insert(30);
+            linkedList.insert(70);
+
+            linkedList.print();
+
+        }
+    }
+
